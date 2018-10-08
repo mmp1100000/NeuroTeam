faf5efa794d2b3606485ddcc3ba8f161aa7854f3
Unknown <marcomunozperez@uma.es>
2018-10-08 22:25:26 +0200

    Añadido sentencias insert de médico

---
+++ insert_medicos.sql
@@ -0,0 +1,8 @@
+INSERT INTO Medico (nombre,apellidos,licencia)  VALUES ("Juan","Lopez Gutierrez",2930485);
+INSERT INTO Medico (nombre,apellidos,licencia)  VALUES ("Felipe","Gomez Maldonado",3940567);
+INSERT INTO Medico (nombre,apellidos,licencia)  VALUES ("Antonio","Gonzalez Godoy",6958493);
+INSERT INTO Medico (nombre,apellidos,licencia)  VALUES ("Jose Antonio","Lopez Wilzinki",2940573);
+INSERT INTO Medico (nombre,apellidos,licencia)  VALUES ("Javier","Casado",3940598);
+INSERT INTO Medico (nombre,apellidos,licencia)  VALUES ("Jaime","Parry",2948567);
+INSERT INTO Medico (nombre,apellidos,licencia)  VALUES ("Andres","Rovira Coronado",2934956);
+
