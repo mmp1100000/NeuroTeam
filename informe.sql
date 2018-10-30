0c8ee55be8d10bd4bc4dcdb79168ce6f57b8e90e
Unknown <marcomunozperez@uma.es>
2018-10-30 12:44:00 +0100

    insert informes pacientes 1

---
+++ informe.sql
@@ -0,0 +1,9 @@
+INSERT INTO Informe (Diagnostico,sintomas,fecha,Medico_has_Paciente_Medico_id,Medico_has_Paciente_Paciente_id)  VALUES ("Huntington","...","2018-05-01",1,1);
+INSERT INTO Informe (Diagnostico,sintomas,fecha,Medico_has_Paciente_Medico_id,Medico_has_Paciente_Paciente_id)  VALUES ("Espina bifida","...","2018-07-09",1,2);
+INSERT INTO Informe (Diagnostico,sintomas,fecha,Medico_has_Paciente_Medico_id,Medico_has_Paciente_Paciente_id)  VALUES ("Alzheimer","...","2018-10-15",2,3);
+INSERT INTO Informe (Diagnostico,sintomas,fecha,Medico_has_Paciente_Medico_id,Medico_has_Paciente_Paciente_id)  VALUES ("Parkinson","...","2017-12-01",2,4);
+INSERT INTO Informe (Diagnostico,sintomas,fecha,Medico_has_Paciente_Medico_id,Medico_has_Paciente_Paciente_id)  VALUES ("Alzheimer","...","2016-05-01",3,5);
+INSERT INTO Informe (Diagnostico,sintomas,fecha,Medico_has_Paciente_Medico_id,Medico_has_Paciente_Paciente_id)  VALUES ("Parkinson","...","2017-02-14",3,6);
+INSERT INTO Informe (Diagnostico,sintomas,fecha,Medico_has_Paciente_Medico_id,Medico_has_Paciente_Paciente_id)  VALUES ("Huntington","...","2018-02-12",4,7);
+INSERT INTO Informe (Diagnostico,sintomas,fecha,Medico_has_Paciente_Medico_id,Medico_has_Paciente_Paciente_id)  VALUES ("Epilepsia","...","2015-04-09",4,8);
+
