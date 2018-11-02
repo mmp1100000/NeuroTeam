86f214d5474344aad363e7afcba1d79181bb3a9f
Unknown <marcomunozperez@uma.es>
2018-11-02 12:48:59 +0100

    Updated xml generator
    now creates an xml file

--- XML generator/main.py
+++ XML generator/main.py
@@ -2,10 +2,12 @@ from mysql_to_xml import MysqlToXml
 from mysql_connector import Connection
 import mysql.connector
 
-test = MysqlToXml(Connection(mysql.connector.connect(
+test = MysqlToXml(
+    Connection(mysql.connector.connect(
         host="127.0.0.1",
         user="root",
         passwd="qwerty",
         database="neuroteam",
         port=3306)),
-        database_name="neuroteam")
+        database_name="neuroteam",
+        path="neuroteam.xml")
